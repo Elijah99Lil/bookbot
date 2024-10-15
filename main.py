9ced02a5cca85b854d0468c4f1361ad3591f5923
bachie <elijah99lil@gmail.com>
2024-10-15 03:54:03 -0600

    V: added 'character_count' to main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+def character_count(text):
+    char_dict = {}
+    for char in text.lower():
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
 def count_words(text):
     words = text.split()
     print(f"There are {len(words)} words lmao. Get hampter'd")
@@ -6,7 +15,8 @@ def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
         count_words(file_contents)
-
+        char_count = character_count(file_contents)
+        print(char_count)
 
 if __name__ == "__main__":        
     main()
