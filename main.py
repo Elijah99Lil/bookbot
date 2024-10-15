c732adb14ada6f4186d2fb3e085275df446af4c6
bachie <elijah99lil@gmail.com>
2024-10-15 02:59:14 -0600

    IV: added 'word_count' to main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+def count_words(text):
+    words = text.split()
+    print(f"There are {len(words)} words lmao. Get hampter'd")
+
+def main():
+    with open('books/frankenstein.txt') as f:
+        file_contents = f.read()
+        count_words(file_contents)
+
+
+if __name__ == "__main__":        
+    main()
