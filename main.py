31968a4e2637a62400252a0f97e5dd0b7ee1a3a2
bachie <elijah99lil@gmail.com>
2024-10-15 04:29:50 -0600

    VI: added 'sort_on' and added to 'main()' to format report

--- main.py
+++ main.py
@@ -7,16 +7,24 @@ def character_count(text):
             char_dict[char] = 1
     return char_dict
 
+def sort_on(entry):
+    return entry["num"]
+
 def count_words(text):
     words = text.split()
-    print(f"There are {len(words)} words lmao. Get hampter'd")
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    print(f"{len(words)} words found in the document:")
 
 def main():
     with open('books/frankenstein.txt') as f:
         file_contents = f.read()
         count_words(file_contents)
         char_count = character_count(file_contents)
-        print(char_count)
+        char_list = [{"char": char, "num": count} for char, count in char_count.items() if char.isalpha()]
+        char_list.sort(reverse=True, key=sort_on)
+        for entry in char_list:
+            print(f"The '{entry['char']}' character was found {entry['num']} times")
+        print(f"--- End report ---")
 
 if __name__ == "__main__":        
     main()
